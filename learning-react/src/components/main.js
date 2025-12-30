9686aa4287fe0ca7dbbd45b84011e523bf3b25fe
OtavioSantanna <otavio.santana085@gmail.com>
2025-12-29 23:17:43 -0300

    update

--- learning-react/src/components/main.js
+++ learning-react/src/components/main.js
@@ -1,10 +1,27 @@
 import React, {Component} from "react";
+import { FaPlus, FaEdit, FaWindowClose } from 'react-icons/fa';
 import './main.css';
 
 export default class Main extends Component{
     state = {
         novaTarefa: '',
+        tarefas: []
     };
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        const {tarefas} = this.state;
+        let { novaTarefa} = this.state;
+        novaTarefa = novaTarefa.trim();
+
+        if(tarefas.indexOf(novaTarefa) != -1) return;
+
+        const novasTarefas = [...tarefas];
+
+        this.setState({
+            tarefas: [...novasTarefas, novaTarefa],
+        })
+    }
 
     handleChange = (e) =>{
         this.setState({
@@ -13,17 +30,31 @@ export default class Main extends Component{
     }
 
     render(){
-        const{ novaTarefa } = this.state;
+        const{ novaTarefa, tarefas } = this.state;
 
         return(
             <div className="main">
                 <h1>{novaTarefa}</h1>
 
-                <form action='#' className="form">
+                <form onSubmit={this.handleSubmit} action='#' className="form">
                     <input onChange={this.handleChange} type="text"
                     value={novaTarefa}></input>
-                    <button type="submit">Enviar</button>
+                    <button type="submit">
+                        <FaPlus/>
+                    </button>
                 </form>
+
+                <ul className="tarefas">
+                    {tarefas.map((tarefas) => (
+                        <li key={tarefas}>
+                            {tarefas}
+                            <div>
+                                <FaEdit className="edit"/>
+                                <FaWindowClose className="delete"/>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
             </div>
         )
     }
